8b044e2054e65524726bf392e7841808ac1c4035
Sebastian Perez <sebassperez11@gmail.com>
2024-05-24 13:06:56 -0300

    Used Json

--- src/js/vehiculos.js
+++ src/js/vehiculos.js
@@ -1,7 +1,27 @@
-const vehiculos = [
+export const vehiculos = [
     {
+        id: 1,
+        model: 'Modelo S',
+        image: 'https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Mega-Menu-Vehicles-Model-S.png'
+    },
+    {
+        id: 2,
+        model: 'Modelo 3',
+        image: 'https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Mega-Menu-Vehicles-Model-3-LHD.png'
+    },
+    {
+        id: 3,
+        model: 'Modelo X',
+        image: 'https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Mega-Menu-Vehicles-Model-X.png'
+    },
+    {
+        id: 4,
         model: 'Modelo Y',
-        price: '299'
-
-    }
+        image: 'https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Mega-Menu-Vehicles-Model-Y.png'
+    },
+    {
+        id: 5,
+        model: 'Cybertruck',
+        image: 'https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Mega-Menu-Vehicles-Cybertruck-1x.png'
+    },
 ]
